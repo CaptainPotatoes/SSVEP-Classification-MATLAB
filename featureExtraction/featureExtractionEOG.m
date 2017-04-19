e0517e7017fd08cf7014a4d78f04fe4d54d56d02
CaptainPotatoes <musasmahmood@gmail.com>
2017-04-19 15:02:52 -0400

    New features, new training method, threshold, etc.

--- featureExtraction/featureExtractionEOG.m
+++ featureExtraction/featureExtractionEOG.m
@@ -1,18 +1,29 @@
 function [ F ] = featureExtractionEOG( samplesX )
 %featureExtraction Summary of this function goes here if I ever feel like
 %writing one up.
-% samplesX = samplesX(:);
+
+% figure(20);hold on; plot(samplesX);
+
+waveletCoef = cwt(samplesX,1:20,'haar');
+Scalogram = abs(waveletCoef.*waveletCoef);
+E = sum(Scalogram(:)); %Energy Level.
+IntegralEst = trapz(samplesX);
+[Max,Imax] = findpeaks(diff(samplesX), 'SORTSTR','descend','NPEAKS',1);
+[Min,Imin] = findpeaks(-diff(samplesX),'SORTSTR','descend','NPEAKS',1);
+Amplitude = abs(Max-Min);
+Velocity = Amplitude/(Imax - Imin);
+Mean = (1/size(samplesX,2))*sum(samplesX);
 T_mean = Wmean(samplesX);
 T_stdv = Wstd(samplesX);
 T_max = Wmax(samplesX);
 T_min = Wmin(samplesX);
-T_countmin_1 = sum(samplesX<-1*10^-5 & samplesX>(-1*10^-4),2);
-T_countmin_2 = WCountMin(samplesX,-1*10^-4);
-T_countmax = WCountMax(samplesX,8.5*10^-5);
+% T_countmin_1 = sum(samplesX<-1*10^-5 & samplesX>(-1*10^-4),2);
+% T_countmin_2 = WCountMin(samplesX,-1*10^-4);
+% T_countmax = WCountMax(samplesX,8.5*10^-5);
 T_Integrate = trapz(samplesX);
 peaks = [];
 T_findpeaks_distX=[];
-[peaks, loc] = findpeaks(samplesX, 'MinPeakHeight', 0.65E-4);
+[peaks, loc] = findpeaks(samplesX, 'MinPeakHeight', 1E-3);
 if isempty(peaks)
     T_count_findpeaks = 0;
     T_findpeaks_distX = 0;
@@ -24,7 +35,8 @@ else
         T_findpeaks_distX = 0;
     end
 end
-% F = horzcat(T_mean, T_stdv, T_max, T_min, T_countmin_1, T_countmin_2, T_countmax, T_Integrate);
-F = horzcat(T_mean, T_stdv, T_max, T_min, T_countmin_1, T_countmin_2, T_countmax, T_Integrate, T_count_findpeaks, T_findpeaks_distX);
+
+F = horzcat(E,IntegralEst,Velocity,Amplitude,Mean,T_mean, T_stdv, T_max, T_min, T_Integrate, T_count_findpeaks, T_findpeaks_distX);
+% hold off;
 end
 
--- trainEOGCSV.m
+++ trainEOGCSV.m
@@ -1,108 +1,132 @@
 %% Shifting Window Method
 clear;clc;close all;
-% DATA = csvread('MarcTestData.csv');
-DATA = csvread('MarcTest3.csv');
-rFB = 500; % Remove From Beginning
-rFE = 250; % Remove From End
-ch1 = DATA(1+rFB:end-rFE,1); %ignore last second
-ch2 = DATA(1+rFB:end-rFE,2);
-ch3 = DATA(1+rFB:end-rFE,3);
+DATA = csvread('FadiTest5.csv');
+rFB = 0; % Remove From Beginning
+rFE = 0; % Remove From End
+for i = 1:3
+    ch(:,i) = DATA(1+rFB:end-rFE,i);
+end
 dataTags = DATA(1+rFB:end-rFE,4);
-recordingLengthSeconds = length(ch1)/250;
+recordingLengthSeconds = length(ch(:,1))/250;
 fprintf('Recording Length: %2.2f seconds \n',recordingLengthSeconds);
 Fs = 250;
 h=1/250;
-t=0:h:length(ch1)/250-h;
-ch1f = eog_h_fcn(ch1,250);
-ch2f = eog_h_fcn(ch2,250);
-ch3f = eog_h_fcn(ch3,250);
-f1 = figure(1); set(f1, 'Position', [100, 100, 1600, 900]); hold on;
-plot(ch1f);plot(ch2f);plot(ch3f);
-temp = abs(diff(ch1f));
-% plot(abs(diff(ch1f)));plot(abs(diff(ch2f)));plot(abs(diff(ch3f)));
-for i=2:length(ch1)
+t=0:h:length(ch)/250-h;
+for i = 1:3
+    chf(:,i) = eog_h_fcn(ch(:,i),250);
+    cutoff(:,i) = abs(diff(customFilt(ch(:,i),Fs,[0.001 10],2)));
+end
+f1 = figure(1); set(f1, 'Position', [100, 100, 1600, 900]); hold on; 
+plot(cutoff);
+for i=2:length(chf)
     if (dataTags(i) ~= dataTags(i-1)) %Signal Change
-        switch (dataTags(i))
-            case 0
-                tag = 'null';
-            case 1
-                tag = 'blink';
-            case 2
-                tag = 'double-blink';
-            case 3
-                tag = 'up';
-            case 4
-                tag = 'left';
-            case 5
-                tag = 'right';
-            case 6
-                tag = 'down';
-            case -1
-                tag = 'ret2center';
-        end
-        text( i, ch1f(i), num2str(dataTags(i)));
-%         text( i, temp(i), num2str(dataTags(i)));
+        text( i, cutoff(i,1), num2str(dataTags(i)) );
     end
 end
+figure(2);
+
 hold off;
 
 %% Set classes:
 seconds = 1; %2 second window
 winLen = seconds*Fs; 
-winFraction = 4;%2.5; %1/4 of a second
-winShift = floor(Fs/winFraction); 
-dataLimit = floor((length(ch1)-winLen)/winLen);
+winFraction = 4; %2.5; %1/4 of a second
+winShift = floor( Fs/winFraction ); 
+dataLimit = floor( (length(ch1)-winLen)/winLen );
 start = 1;
-numCh = 4;
-Window = cell( seconds*winFraction*dataLimit - 1, numCh);
-assignedClass = zeros( seconds*winFraction*dataLimit - 1, 1);
+numCh = 3;
+Window = cell( seconds*winFraction*dataLimit - 1, numCh );
+WindowTags = cell( 1,1 ); 
+assignedClass = zeros( seconds*winFraction*dataLimit - 1, 1 );
 figNum = 2;
 fH = figure(figNum); 
-set(fH, 'Position', [100, 100, 1200, 900]);
-minPeakProm = 0.5E-4;
+set(fH, 'Position', [100, 100, 1600, 900]);
+fH2 = figure(3);
+set(fH2, 'Position', [2600, 100, 1600, 900]);
+YLIM = [-0.005, 0.01];
 iterations = seconds*winFraction*dataLimit;
-disp(iterations);
+absDiffThreshold = 2.5E-5;
+fprintf('Iterations: %2.2f \r\n',iterations);
+cnt = 1;
+
 for i = 1 : iterations
     start = 1 + winShift*(i-1);
     winEnd = start + winLen-1;
     fprintf('Current index = [%d to %d]\r\n',start, winEnd);
-    Window{i,1} = ch1( start : start + winLen-1 );              % set values:
-    Window{i,2} = ch2( start : start + winLen-1 );
-    Window{i,3} = ch3( start : start + winLen-1 );
-    %%%%% TODO: CHANGE FILTER:
-    ch1f = eogcfilt( Window{i,1} ); 
-    ch2f = eogcfilt( Window{i,2} );
-    ch3f = eogcfilt( Window{i,3} );
-    [p, l] = findpeaks(ch1f, 'MinPeakHeight',minPeakProm);
-    [p1, l1] = findpeaks(ch2f, 'MinPeakHeight',minPeakProm);
-    figure(2)
-    hold on;
-        plot(ch1f),ylim([-2.5E-4 2.5E-4]); 
-        plot(ch2f),ylim([-2.5E-4 2.5E-4]);
-        plot(ch3f),ylim([-2.5E-4 2.5E-4]);
-        plot(l,p,'-*r'); 
-        plot(l1,p1,'-*c');
-    hold off;
-    %%%% TODO: Make separate EOG Classifier.
-    getClass = [];
-    while isempty(getClass)
-        commandwindow;
-        getClass = input('Enter an integer value!\n');
-        if isempty(getClass)
-            getClass = 0; 
-        end
+    for c = 1:3
+        Window{i,c} = ch(start : start + winLen-1,c);
     end
-    tY(i,1) = getClass;
-    F_ch1(i,:) = featureExtractionEOG( ch1f' );
-    F_ch2(i,:) = featureExtractionEOG( ch2f' );
-    F_ch3(i,:) = featureExtractionEOG( ch3f' );
+    WindowTags{i} = dataTags( start : start + winLen-1 );
+    
+    %%%%% TODO: CHANGE FILTER: 
+    %%%%%%%%%% TODO: CHANGE TO FOR LOOPS.
+    tch1f = eog_h_fcn(Window{i,1},250);
+    tch2f = eog_h_fcn(Window{i,2},250);
+    tch3f = eog_h_fcn(Window{i,3},250);
+    
+    dtch1f = abs(diff(tch1f));
+    dtch2f = abs(diff(tch2f));
+    dtch3f = abs(diff(tch3f));
+
+    th_ch1 = abs(diff(tch1f,2));
+    th_ch2 = abs(diff(tch2f,2));
+    th_ch3 = abs(diff(tch3f,2));
+    
+    figure(2); hold on;
+        plot(tch1f),ylim(YLIM); 
+        plot(tch2f),ylim(YLIM); title('Filtered EOG Data');
+        plot(tch3f),ylim(YLIM); hold off;
+    figure(3);hold on; 
+        plot(th_ch1);
+        plot(th_ch2); title('ABS(DIFF6(filteredData))');
+        plot(th_ch3),ylim([0 1E-4]); hold off;
+    
+    thresholdCheck = (max(dtch1f)>absDiffThreshold || max(dtch2f)>absDiffThreshold || max(dtch3f)>absDiffThreshold);
+    if (thresholdCheck) 
+        tagsPresent = unique(WindowTags{i},'stable')
+        %%%% TODO: If surpasses threshold THEN classify and store data.
+        getClass = [];
+        while isempty(getClass)
+            commandwindow;
+            getClass = input('Enter an integer value!\n');
+            if isempty(getClass)
+                getClass = 0; 
+            end
+        end
+        tY(cnt,1) = getClass;
+        %%%% TODO: Make separate EOG Classifier/Feature Extraction. 
+        F_ch1(cnt,:) = featureExtractionEOG( tch1f' );
+        F_ch2(cnt,:) = featureExtractionEOG( tch2f' );
+        F_ch3(cnt,:) = featureExtractionEOG( tch3f' );
+        cnt = cnt + 1;
+    end
     clf(figNum);
+    clf(3);
 end
-%horzcat: 32 features (8/ch)
+
+%horzcat: 36 features (12/ch)
 tX = [F_ch1 F_ch2 F_ch3];
-%
+
 
 
 %% % % % TRAIN CLASSIFIER, KNN :: filename('knnclassification')
 
 % Import features with extractions. 
+%         switch (dataTags(i))
+%             case 0
+%                 tag = 'null';
+%             case 1
+%                 tag = 'blink';
+%             case 2
+%                 tag = 'double-blink';
+%             case 3
+%                 tag = 'up';
+%             case 4
+%                 tag = 'left';
+%             case 5
+%                 tag = 'right';
+%             case 6
+%                 tag = 'down';
+%             case -1
+%                 tag = 'ret2center';
+%         end
